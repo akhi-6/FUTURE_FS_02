8d66a43f7c640021c3e511f8cf9e08cbd9168d0e
akhi-6 <bodduriakhila0906@gmail.com>
2026-04-26 19:05:32 +0530

    Added frontend build

--- backend/server.js
+++ backend/server.js
@@ -7,11 +7,10 @@ const app = express();
 
 // Middleware
 app.use(cors());
-app.use(express.json());
+app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-// Test route
-app.get("/", (req, res) => {
-  res.send("Backend is running 🚀");
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
 });
 
 // Routes
